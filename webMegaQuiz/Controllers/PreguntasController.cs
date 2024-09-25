7c6a140ab1601e0c51f14d7875050a5d197feb55
LUJAAA <javier.olivareslr@uanl.edu.mx>
2024-09-24 18:47:00 -0600

    Agregar archivos de proyecto.

---
+++ webMegaQuiz/Controllers/PreguntasController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using webMegaQuiz.Models;
+
+namespace webMegaQuiz.Controllers
+{
+    public class PreguntasController : Controller
+    {
+        /////////////////////////////////////
+        // Declaro un cliente  //
+        /////////////////////////////////////
+        private readonly HttpClient _httpClient;
+
+        /////////////////////////////////////
+        // Creo el contructor  //
+        /////////////////////////////////////
+        public PreguntasController(IHttpClientFactory httpClientFactory)
+        {
+            // Inicializo el cliente y le paso la url basica
+            _httpClient = httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri("http://localhost:5212/");
+        }
+
+
+        public async Task<IActionResult> Listar()
+        {
+            // Llamamos a la API //
+            var respuesta = await _httpClient.GetAsync("api/preguntas/ListaPreguntas");
+            // Validamos si la respuesta fue exitosa 
+            if (respuesta.IsSuccessStatusCode)
+            {
+                // obtengo y decodifico la respuesta
+                var respuestaContenido = await respuesta.Content.ReadAsStringAsync();
+                //var preguntas = JsonConvert.DeserializeObject<List<PreguntaModels>>(respuestaContenido);
+                //var preguntas = JsonConvert.DeserializeObject<PreguntaModels>(respuestaContenido);
+                var preguntas = JsonConvert.DeserializeObject<IEnumerable<PreguntaModels>>(respuestaContenido);
+                // devuelvo una vista
+                return View("Listar", preguntas);
+            }
+            return View(new List<PreguntaModels>());
+        }
+
+        public async Task<IActionResult> ObtenerUnaPregunta(int id)
+        {
+            // Llamamos a la API //
+            var respuesta = await _httpClient.GetAsync("api/preguntas/ObtenerPregunta/"+id.ToString());
+            if (respuesta.IsSuccessStatusCode)
+            {
+                // obtengo y decodifico la respuesta
+                var respuestaContenido = await respuesta.Content.ReadAsStringAsync();
+                var pregunta = JsonConvert.DeserializeObject<PreguntaModels>(respuestaContenido);
+                // devuelvo una vista
+                return View(pregunta);
+            }
+            else
+            {
+                return View("Inicio");
+            }
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ webMegaQuiz/Models/PreguntaModels.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Diagnostics.Metrics;
+using System.Net.Sockets;
+
+namespace webMegaQuiz.Models
+{
+    public class PreguntaModels
+    {
+        // Es la tabla que esta en la base de datos
+        public int idPregunta {  get; set; }
+        public string pregunta {  get; set; } = null!;
+        public string rCorrecta { get; set; } = null!;
+        public string rIncorrecta1 { get; set; } = null!;
+        public string rIncorrecta2 { get; set; } = null!;
+        public string rIncorrecta3 { get; set; } = null!;
+        public string rIncorrecta4 { get; set; } = null!;
+        public string rIncorrecta5 { get; set; } = null!;
+        public int estado { get; set; }
+        public int dificultad { get; set; }
+
+    }
+}
